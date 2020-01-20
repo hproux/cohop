8228736758995720d009faef3d8798b46e53b495
gciliberti <g.ciliberti@protonmail.com>
2020-01-20 15:30:48 +0100

    verification session

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,21 @@ export default {
     loggedMember: function () {
       return this.$store.state.isLogged ? true : false;
     }
-}
+  },
+  created : function(){
+    if(this.$store.state.token){
+      axios.get("members/"+this.$store.state.id+"/signedin")
+      .then((response)=>{
+        console.log(response)
+      })
+      .catch((error)=>{
+        this.$router.push('login')
+      })
+    }else{
+      this.$router.push('login')
+    }
+
+  }
+
 }
 </script>
--- src/components/Members.vue
+++ src/components/Members.vue
@@ -18,19 +18,6 @@
         </tbody>
       </table>
     </div>
-    <div class="card-content column">
-      <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-        <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-        <br>
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-      </div>
-    </div>
-    <footer class="card-footer">
-      <a href="#" class="card-footer-item">Save</a>
-      <a href="#" class="card-footer-item">Edit</a>
-      <a href="#" class="card-footer-item">Delete</a>
-    </footer>
   </div>
 </template>
 
@@ -46,7 +33,7 @@ export default {
   },
   data(){
     return {
-      members : null,
+      members : [],
     }
   },
   created: function(){
